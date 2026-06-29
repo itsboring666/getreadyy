f3015868d744a4a0074cd4ff2eb681a5fb02acfc
KathirChelvan <ki8460@srmist.edu.in>
2026-06-29 22:02:58 +0530

    Fix hardcoded MAIL_FROM_ADDRESS

--- config/mail.php
+++ config/mail.php
@@ -112,8 +112,8 @@ return [
     */
 
     'from' => [
-        'address' => 'onboarding@resend.dev',
-        'name' => 'GET READY Store',
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
 ];
